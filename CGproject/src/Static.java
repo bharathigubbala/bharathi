c5fb1bc252a1f8f40ae1e4915d07268f80a2c4fe
acer <acer@192.168.0.101>
2022-06-25 12:09:47 +0530

    staticvariable

---
+++ CGproject/src/Static.java
@@ -0,0 +1,13 @@
+ 
+public class Static
+{
+	 private static double salary;
+	 public static final String EMPLOYEE = "Employee ";
+	public static void main(String[] args)
+	{
+		 salary = 2554;
+	     System.out.println(EMPLOYEE + "average salary:" + salary);
+	  }
+
+	}
+
