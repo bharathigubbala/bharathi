1dbed408751d821145e988e778e1d3096529a561
acer <acer@192.168.0.105>
2022-06-27 11:52:10 +0530

    switch string

---
+++ CGproject/src/Sstring.java
@@ -0,0 +1,26 @@
+
+public class Sstring {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String name = "BHARATHI";
+
+		switch (name.toLowerCase()) 
+		{
+		case "book":
+			System.out.println("bharahhi");
+			break;
+		case "team":
+			System.out.println("team is svkp");
+			break;
+		case "publisherr":
+			System.out.println("svkp");
+			break;
+		default:
+			System.out.println("Invalid entry");
+			break;
+	}
+}
+
+	}
+
