26787eb2980fd62dfe7ca474a88fb07879f06b49
acer <acer@192.168.0.105>
2022-06-27 11:27:21 +0530

    decition&operators

---
+++ CGproject/src/Ifopdeci.java
@@ -0,0 +1,32 @@
+
+public class Ifopdeci {
+	public static void main(String[] args) {
+	 int x = 23;
+     int y = 27;
+	  
+	  if(!(x < y) || (x == y))
+	  { 
+		  System.out.println("Condition is TRUE");
+	  }
+	  else
+	  { 
+		  System.out.println("Condition is FALSE"); 
+	}
+	 
+
+int ageOfBoy = 27;
+	int ageOfGirl = 23;
+
+	if ((ageOfBoy >= 25) && (ageOfGirl >= 21)) 
+	{
+		System.out.println("ready to get married!");
+	} 
+	else 
+	{
+		System.out.println("Wait for it kiddo!");
+	}
+
+}
+
+
+}
