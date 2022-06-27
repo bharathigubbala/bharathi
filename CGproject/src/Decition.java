4639073d1d75a07e220e56079a61b52925615d2b
acer <acer@192.168.0.105>
2022-06-27 11:22:12 +0530

    if with decition

---
+++ CGproject/src/Decition.java
@@ -0,0 +1,20 @@
+
+public class Decition {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a = 49, b = 23;
+		
+      if(a==b)
+        {
+     	   System.out.println("true");
+        }
+        else
+        {
+     	   System.out.println("false");
+        }
+
+
+	}
+
+}
