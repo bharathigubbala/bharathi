bb237f47f1086264af3707caa6c6c5ae3a74ecf5
acer <acer@192.168.0.105>
2022-06-27 11:16:58 +0530

    Ifwithoperators

---
+++ CGproject/src/If.java
@@ -0,0 +1,19 @@
+
+public class If {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int x = 4;
+        if(x < 5)
+        {
+      	  System.out.println("Given number is less than 5");
+      	  
+        }
+        else
+        {
+        System.out.println("Given number is not less than 5"); 
+
+	}
+
+}
+}
