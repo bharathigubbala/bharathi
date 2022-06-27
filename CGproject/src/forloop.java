e051cc4079bd71319f386523344fd68b8e0f4877
acer <acer@192.168.0.105>
2022-06-27 11:55:04 +0530

    forloop example

---
+++ CGproject/src/forloop.java
@@ -0,0 +1,27 @@
+
+public class forloop {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int number = 5;
+		boolean isPrime = true;
+		for(int i = 2; i < number/2; i++)
+		{
+			if(number % i == 0)
+			{
+				isPrime = false;
+			}
+		}
+		if(isPrime == true)
+		{
+System.out.println("The number is a prime number");
+		}
+		else
+		{
+System.out.println("The number is not a prime number");
+		}
+		
+
+	}
+
+	}
