5f3f148d507cb5114a5d217e167358b57d93d1cf
acer <acer@192.168.0.105>
2022-06-27 11:40:04 +0530

    Ifladder

---
+++ CGproject/src/Ifladder.java
@@ -0,0 +1,36 @@
+public class Ifladder {
+
+	public static void main(String[] args)
+	{
+        int x = 20;
+        
+		 if (x == 20) 
+		  { 
+		  if (x >= 19) 
+		  {
+		  	System.out.println("X is greater than 19"); 
+		   }
+		  else 
+		  {
+		  System.out.println("X is smaller than 19"); 
+		  } 
+		 }
+		
+		if(x == 20)
+		{
+			System.out.println("X equals to 20");
+		}
+		else if(x > 20)
+		{
+			System.out.println("X is greater to 20");		
+		}
+		else 
+		{
+			System.out.println("X is smaller than 20");
+		}
+
+		// TODO Auto-generated method stub
+
+	}
+
+}
