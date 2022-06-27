7d0f45264c356291b71d5b33ad8062d32894ebcb
acer <acer@192.168.0.105>
2022-06-27 12:16:20 +0530

    while example

---
+++ CGproject/src/While.java
@@ -0,0 +1,15 @@
+
+public class While {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i=3;
+		while(i<=11)
+		{
+			i++;
+			System.out.println(i);
+		}
+
+	}
+
+}
