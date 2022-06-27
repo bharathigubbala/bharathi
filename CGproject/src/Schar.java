77dd4f70e375e53fd74847969c602e2ff62cbd8c
acer <acer@192.168.0.105>
2022-06-27 11:47:06 +0530

    SWITCH CHAR

---
+++ CGproject/src/Schar.java
@@ -0,0 +1,25 @@
+
+public class Schar {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		char x = 'a';
+		switch (x) 
+		{
+		case 'a' :  
+			System.out.println("The value of x is a");
+			break;
+		case 'b':
+			System.out.println("The value of x is b");
+			break;
+		case 'c':
+			System.out.println("The value of x is c");
+			break;
+		default:
+	System.out.println("The value of x is other than a,b,c");
+			break;
+		}
+
+	}
+
+}
