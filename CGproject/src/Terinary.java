93e8942a0ef09a3ca6525638d120c10bc43c532b
acer <acer@192.168.0.105>
2022-06-27 11:33:39 +0530

    terinary&assaignment

---
+++ CGproject/src/Terinary.java
@@ -0,0 +1,23 @@
+
+public class Terinary {
+
+	public static void main(String[] args)
+	{
+		// TODO Auto-generated method stub
+	int x = 11;
+		
+		x = (11 == x) ? 1 : 0;   //  condition ?: True:False
+		
+		System.out.println(x);
+		          x += 5;           // x = x + 5
+				   x -= 5; // x = x - 5
+				    x *= 5; // x = x * 5
+				    x /= 4; // x = x / 4
+				    x %= 5;   // x = x % 5
+				System.out.println(x);
+
+		
+		
+	}
+
+}
