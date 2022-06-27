0123147e961c3a73af2fcf316ad640a2beefa2fe
acer <acer@192.168.0.105>
2022-06-27 11:12:04 +0530

    Arithmeticoperator

---
+++ CGproject/src/Arithmetic.java
@@ -0,0 +1,16 @@
+
+public class Arithmetic {
+
+	public static void main(String[] args) {
+		int x = 29, y=50;
+		System.out.println(x+y);
+		System.out.println(x-y);
+		System.out.println(x%y);
+		System.out.println(--x); 
+		System.out.println(++y); 
+
+	
+
+	}
+
+}
