e031e0f2d89cd35ce4e8b9ff69e81e1fc997a5c4
acer <acer@TMP249-G2-M.bbrouter>
2022-07-08 11:32:29 +0530

    swapping

---
+++ CGproject/src/Swaping.java
@@ -0,0 +1,19 @@
+
+public class Swaping 
+{
+
+	public static void main(String[] args)
+	{
+	int x=29,y=40,temp;
+System.out.println("Before swapping the  Two numbers");
+System.out.println("x="+x);
+System.out.println("y="+y);
+temp=x;
+x=y;		
+y=temp;
+System.out.println("After Swapping the Two numbers are:");
+System.out.println("x="+x);
+System.out.println("y="+y);
+
+}
+}
